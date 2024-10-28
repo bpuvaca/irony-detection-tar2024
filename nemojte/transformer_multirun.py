78185b7b140bed4e372d3ce650a407d52a599a42
Bojan Puvača <bpuvaca@gmail.com>
2024-10-28 13:39:19 +0100

    slight changes to main function arguments

--- nemojte/transformer_multirun.py
+++ nemojte/transformer_multirun.py
@@ -4,8 +4,8 @@ datasets = ['irony', 'sarcasm', 'mix']
 models = ['roberta', 'bertweet', 'bert']
 params_suffix = ''
 
-for i in range(2,6):
+for i in range(1,2):
     print("======================================= RUN", i, " =======================================")
     for model in models:
         for dataset in datasets:
-            train_and_evaluate(dataset, model, None, f"{model}_{dataset}_{i}{params_suffix}", eval_on='irony sarcasm mix')
+            train_and_evaluate(dataset, model, None, f"{model}_{dataset}_{i}{params_suffix}", 'irony sarcasm mix', False, False)
