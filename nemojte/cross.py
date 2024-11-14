08758817972946ca9541e92a74af48c87b05b6ad
florijan127 <florijan.sandalj@gmail.com>
2024-11-14 09:59:13 +0100

    fixing crossval

--- nemojte/cross.py
+++ nemojte/cross.py
@@ -1,8 +1,8 @@
 
 from transformer_main import cross_validate
 
-for model in ['bertweet', 'roberta', 'bert']:            
+for model in ['bertweet']:#, 'roberta', 'bert']:            
     for train in ["isarcasm_sarc_crossval", "semeval_polarity_crossval"]:
         for valid in ["isarcasm_sarc_crossval", "semeval_polarity_crossval"]:
-                cross_validate(train, model, eval_on=(None if train==valid else valid), save_to=None, folds=7)
+                cross_validate(train, model, eval_on=(None if train==valid else valid), save_to=None, folds=5)
 
