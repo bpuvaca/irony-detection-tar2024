da4f2c45ad9c2edb433e3c02054ebea8b516e4ce
florijan127 <florijan.sandalj@gmail.com>
2024-11-18 14:34:05 +0100

    fixing save preds

--- nemojte/cross.py
+++ nemojte/cross.py
@@ -3,7 +3,7 @@ from transformer_main import train_and_cross_validate
 
 for model in ['bertweet', 'roberta', 'bert']:            
     for ds in ["sarcasm_crossval", "polarity_crossval", "sarcasm_mix_crossval", "irony_mix_crossval"]:
-        train_and_cross_validate(ds, model, return_all_preds=True, folds=5, save_params=True)
+        train_and_cross_validate(ds, model, return_all_preds=True, folds=5, save_params=True, epochs=10)	
 
 
 
