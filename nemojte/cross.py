7fde43fbb3e649e6af54f232ca8552fd81c47d14
ivan.unkovic@fer.hr <iu53411@fer.hr>
2024-11-27 13:53:51 +0100

    add

--- nemojte/cross.py
+++ nemojte/cross.py
@@ -29,12 +29,12 @@ from transformer_main import train_and_cross_validate, cross_validate
 #                     load_from=f"crossval/{model}/{train_ds}", 
 #                     return_all_preds=True, folds=5)#, fold_test_dataset=test_ds.endswith("crossval"))
 
-for model in ['bertweet', 'roberta', 'bert']:            
-   for ds in ["mix"]:
-       train_and_cross_validate(ds, model, return_all_preds=True, folds=5, save_params=True, epochs=5)	
+# for model in ['bertweet', 'roberta', 'bert']:            
+#    for ds in ["mix"]:
+#        train_and_cross_validate(ds, model, return_all_preds=True, folds=5, save_params=True, epochs=5)	
 
 
-for model in ['bertweet', 'roberta', 'bert']:            
+for model in ['bertweet', 'roberta']:#, 'bert']:            
     for train_ds in ["mix"]:
         for test_ds in ["sarcasm", "polarity", "irony", "other"]:
             if train_ds != test_ds:
