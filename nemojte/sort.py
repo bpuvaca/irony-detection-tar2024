fb9757b6d2ef69bd2f23667e58dafd5c63a33317
ivan.unkovic@fer.hr <iu53411@fer.hr>
2025-01-31 11:12:46 +0100

    sorted bf_predictions

--- nemojte/sort.py
+++ nemojte/sort.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv("lowest_correctness/all_predictions/roberta_trained_on_semeval_mix_4epoch.csv")
+df = pd.read_csv("lowest_correctness/bf_predictions/roberta_trained_on_bf_irony_roberta_4epoch.csv")
 df_sorted = df.sort_values(by="mean_correctness", ascending=True)
-df_sorted.to_csv("lowest_correctness/all_predictions/roberta_trained_on_semeval_mix_4epoch.csv", index=False)
+df_sorted.to_csv("lowest_correctness/bf_predictions/roberta_trained_on_bf_irony_roberta_4epoch.csv", index=False)
