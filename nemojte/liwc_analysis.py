1acd54ed124b6d54af3a80537fb397c427fce2a1
florijan127 <florijan.sandalj@gmail.com>
2025-02-03 11:28:55 +0100

    liwc stvari

--- nemojte/liwc_analysis.py
+++ nemojte/liwc_analysis.py
@@ -3,16 +3,46 @@ import numpy as np
 import sys
 
 
-def summarise_liwc(model, train_ds, test_ds, path):
+def summarise_liwc(model, train_ds, test_ds, path, to_drop):
     
     path = f'{path}/{model}/{train_ds}/{test_ds}'
 
     summary = "Analytic	Clout	Authentic	Tone".split()
 
-    to_drop = "index	dataset	tweet	label	prediction	probability	Segment	WC	Analytic	Clout	Authentic	Tone	WPS".split()
+    
 
 
     with open(f'liwc_summary_{model}_{train_ds}_{test_ds}.txt', 'w') as sys.stdout:
+        print(path)
+        for f in ['fp', 'fn', 'tp', 'tn']:
+            data = pd.read_csv(f'{path}/liwc_{f}.csv')
+            print(f'for {f} size = {len(data)}')
+            print(f'average word count = {np.mean(data["WC"])}')
+            print(f'average wps = {np.mean(data["WPS"])}')
+            for col in summary:
+                print(f'{col} = {np.mean(data[col])} +- {np.std(data[col])}')
+            columns = data.columns.drop(to_drop)
+            other = data[columns]
+            values = []
+            for col in other.columns:
+                values.append(np.mean(other[col]))
+            
+            sorted_values = sorted(zip(other.columns, values), key=lambda x: x[1], reverse=True)
+            
+            print('top 10 features in other columns by average:')
+            for i in range(10):
+                print(f'{sorted_values[i]} +- {np.std(other[sorted_values[i][0]])}')
+            
+            print('\n\n')
+
+def summarise_liwc_cart(model, train_ds, test_ds, path):
+    to_drop = "label	mean_confidence	variability	mean_correctness	correctnesses".split()
+
+    path = f'{path}/{model}/{train_ds}/{test_ds}'
+
+    summary = "Analytic	Clout	Authentic	Tone".split()
+
+    with open(f'liwc_cart_summary_{model}_{train_ds}.txt', 'w') as sys.stdout:
         print(path)
         for f in ['fp', 'fn', 'tp', 'tn']:
             data = pd.read_csv(f'{path}/liwc_{f}.csv')
@@ -36,8 +66,15 @@ def summarise_liwc(model, train_ds, test_ds, path):
             print('\n\n')
 
 
-summarise_liwc('bertweet', 'irony_ds', 'irony_ds', '../preds/crossval4')
-summarise_liwc('bertweet', 'sarcasm_ds', 'sarcasm_ds', '../preds/crossval4')
-summarise_liwc('bertweet', 'semeval_mix_ds', 'semeval_mix_ds', '../preds/crossval4')
-summarise_liwc('roberta', 'sarcasm_ds', 'sarcasm_ds', '../preds/crossval4')
+to_drop = "index	dataset	tweet	label	prediction	probability	Segment	WC	Analytic	Clout	Authentic	Tone	WPS".split()
+summarise_liwc('bertweet', 'irony_ds', 'irony_ds', '../preds/crossval4', to_drop)
+summarise_liwc('bertweet', 'sarcasm_ds', 'sarcasm_ds', '../preds/crossval4', to_drop)
+summarise_liwc('bertweet', 'semeval_mix_ds', 'semeval_mix_ds', '../preds/crossval4', to_drop)
+summarise_liwc('roberta', 'sarcasm_ds', 'sarcasm_ds', '../preds/crossval4', to_drop)
+
+
+
+
+
+# summarise_liwc('roberta', 'semeval_mix', 'irony_ds', 'lowest_correctness/all_predictions', to_drop)
 
--- nemojte/prepare_for_liwc.py
+++ nemojte/prepare_for_liwc.py
@@ -21,6 +21,19 @@ def create_liwc_data(dir_path, models, dsets):
                 tn.to_csv(f'{dir_path}/{model}/{train_ds}/{test_ds}/tn.csv', index=False)
 
 models = ['roberta', 'bert', 'bertweet']
-dsets = ['semeval_mix_ds', 'sarcasm_ds', 'irony_ds']
+# dsets = ['semeval_mix_ds', 'sarcasm_ds', 'irony_ds']
 
-create_liwc_data('../preds/crossval4', models, dsets)
+# create_liwc_data('../preds/crossval4', models, dsets)
+
+
+def prepare_cart_for_liwc(dir_path, model, dsets):
+    for model in models:
+        for train_ds in dsets:
+            path = f'{dir_path}/{model}_trained_on_{train_ds}_4epoch.csv'
+            data = pd.read_csv(path)
+            lowest = data[data['mean_correctness'] <= 0.5]
+            highest = data[data['mean_correctness'] > 0.5]
+            lowest.to_csv(f'{dir_path}/{model}_trained_on_{train_ds}_4epoch_lowest.csv', index=False)
+            highest.to_csv(f'{dir_path}/{model}_trained_on_{train_ds}_4epoch_highest.csv', index=False)
+            
+prepare_cart_for_liwc('lowest_correctness/all_predictions', models, ['semeval_mix', 'irony', 'sarcasm'])
