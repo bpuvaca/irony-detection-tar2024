6c6dedbcb12f1183294d8171cd0b33c37c3c8f1d
florijan127 <florijan.sandalj@gmail.com>
2025-02-10 01:19:05 +0100

    f1 rezultati prije zadnjeg sastanka

--- nemojte/f1_vs.py
+++ nemojte/f1_vs.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, accuracy_score
 import numpy as np
 import os
 
@@ -7,9 +7,11 @@ for model in ['bertweet', 'roberta', 'bert']:
     train_ds = "irony_sarcasm_ds"
     test_ds = "irony_sarcasm_ds" 
     f1s = []
+    accs = []
     for k in range(1, 6):
         results = pd.read_csv(f"../preds/crossval4/{model}/{train_ds}/{test_ds}/{model}_trained_on_{train_ds}_evaluated_on_{test_ds}_fold_{k}.csv")
         f1s.append(f1_score(results['label'], results['prediction']))
+        accs.append(accuracy_score(results['label'], results['prediction']))
     average_f1 = sum(f1s) / 5
     stdev = np.std(f1s)
-    print(f"for {model}, f1 score on irony_vs_sarcasm_ds is {average_f1:.2f} ± {stdev:.2f}")
+    print(f"for {model}, acc = {np.mean(accs):.2f} +- {np.std(accs):.2f} f1 score on irony_vs_sarcasm_ds is {average_f1:.2f} ± {stdev:.2f}")
