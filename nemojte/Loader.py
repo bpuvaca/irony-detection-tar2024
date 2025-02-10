a0203cf90df9dd342a9a0768a8cd47598b9e120a
florijan127 <florijan.sandalj@gmail.com>
2025-02-10 13:13:26 +0100

    add isarcasm path

--- nemojte/Loader.py
+++ nemojte/Loader.py
@@ -52,6 +52,8 @@ file_path_dict = {
     "valid_semeval_mix": "../datasets/crossval/semeval_mix.csv",
     "test_semeval_mix": "../datasets/crossval/semeval_mix.csv",
     
+    "train_isarcasm_mix": "../datasets/crossval/isarcasm_mix.csv",
+    
     
     "train_sarcasm_mix": "../datasets/crossval/sarcasm_mix.csv",
     
