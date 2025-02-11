85c7e883dc043936ebad9f56486e6978b6e0a6e4
florijan127 <florijan.sandalj@gmail.com>
2025-02-11 16:49:51 +0100

    liwc svm without summary

--- nemojte/f1_vs.py
+++ nemojte/f1_vs.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import f1_score, accuracy_score, recall_score
 import numpy as np
 import os
 
@@ -8,22 +8,32 @@ for model in ['bertweet', 'roberta', 'bert']:
     test_ds = "irony_sarcasm_ds" 
     f1s = []
     accs = []
+    recalls = []
     for k in range(1, 6):
         results = pd.read_csv(f"../preds/crossval4/{model}/{train_ds}/{test_ds}/{model}_trained_on_{train_ds}_evaluated_on_{test_ds}_fold_{k}.csv")
         f1s.append(f1_score(results['label'], results['prediction']))
         accs.append(accuracy_score(results['label'], results['prediction']))
+        recalls.append(recall_score(results['label'], results['prediction']))
     average_f1 = sum(f1s) / 5
     stdev = np.std(f1s)
-    print(f"for {model}, acc = {np.mean(accs):.2f} +- {np.std(accs):.2f} f1 score on irony_vs_sarcasm_ds is {average_f1:.2f} ± {stdev:.2f}")
+    print(f"for {model} on {train_ds} vs {test_ds}")
+    print(f'  acc = {np.mean(accs):.2f} +- {np.std(accs):.2f}')
+    print(f'  recall = {np.mean(recalls):.2f} +- {np.std(recalls):.2f}')
+    print(f'  f1 = {average_f1:.2f} ± {stdev:.2f}')
     
     train_ds = "irony_sarcasm"
     test_ds = "irony_sarcasm" 
     f1s = []
     accs = []
+    recalls = []
     for k in range(1, 6):
         results = pd.read_csv(f"../preds/crossval4/{model}/{train_ds}/{test_ds}/{model}_trained_on_{train_ds}_evaluated_on_{test_ds}_fold_{k}.csv")
         f1s.append(f1_score(results['label'], results['prediction']))
         accs.append(accuracy_score(results['label'], results['prediction']))
+        recalls.append(recall_score(results['label'], results['prediction']))
     average_f1 = sum(f1s) / 5
     stdev = np.std(f1s)
-    print(f"for {model}, acc = {np.mean(accs):.2f} +- {np.std(accs):.2f} f1 score on irony_vs_sarcasm is {average_f1:.2f} ± {stdev:.2f}")
+    print(f"for {model} on {train_ds} vs {test_ds}")
+    print(f'  acc = {np.mean(accs):.2f} +- {np.std(accs):.2f}')
+    print(f'  recall = {np.mean(recalls):.2f} +- {np.std(recalls):.2f}')
+    print(f'  f1 = {average_f1:.2f} ± {stdev:.2f}')
--- nemojte/liwc_svm.py
+++ nemojte/liwc_svm.py
@@ -9,22 +9,28 @@ from sklearn.metrics import f1_score
 df = pd.read_csv("../datasets/LIWC/liwc_sarcasm_parsed_filtered.csv")
 df = df.drop(columns=["tweet"])
 
-X = df.iloc[:, 1:].values
-y = df.iloc[:, 0].values 
+irony = pd.read_csv('../results/LIWC/datasets/liwc_irony_parsed.csv', sep=',').drop(columns=['Analytic','Clout','Authentic','Tone'])
+sarcasm = pd.read_csv('../results/LIWC/datasets/liwc_sarcasm_parsed.csv', sep=',').drop(columns=['Analytic','Clout','Authentic','Tone'])
 
-imputer = SimpleImputer(strategy="mean")
-X = imputer.fit_transform(X)
+for name, df in zip(['irony', 'sarcasm'], [irony, sarcasm]):
 
-scaler = StandardScaler()
-X = scaler.fit_transform(X)
+    X = df.iloc[:, 1:].values
+    y = df.iloc[:, 0].values 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    imputer = SimpleImputer(strategy="mean")
+    X = imputer.fit_transform(X)
 
-svm = SVC(kernel="rbf", C=1.0, gamma="scale")
-svm.fit(X_train, y_train)
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
 
-accuracy = svm.score(X_test, y_test)
-print(f"Test Accuracy: {accuracy:.2f}")
-y_pred = svm.predict(X_test)
-f1 = f1_score(y_test, y_pred, average="weighted")
-print(f"F1 Score: {f1:.2f}")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    svm = SVC(kernel="rbf", C=1.0, gamma="scale")
+    svm.fit(X_train, y_train)
+
+    accuracy = svm.score(X_test, y_test)
+    print(f"{name} SVM")
+    print(f"Test Accuracy: {accuracy:.2f}")
+    y_pred = svm.predict(X_test)
+    f1 = f1_score(y_test, y_pred, average="weighted")
+    print(f"F1 Score: {f1:.2f}")
